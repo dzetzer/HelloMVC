4dc3076f5ce4178bf39589140b997bb490b94e9e
Dillon Zetzer <dillon.zetzer@sbcglobal.net>
2019-10-01 06:36:31 -1100

    first commit

---
+++ HelloMVC.Tests/HomeControllerTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace HelloMVC.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
