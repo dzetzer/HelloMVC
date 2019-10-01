7538f5beb079ee3330de6056d2498f0852d0cdd8
Dillon Zetzer <dillon.zetzer@sbcglobal.net>
2019-10-01 07:19:21 -1100

    added controller and tests

--- HelloMVC.Tests/HomeControllerTests.cs
+++ HelloMVC.Tests/HomeControllerTests.cs
@@ -1,3 +1,5 @@
+using HelloMVC.Controllers;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using Xunit;
 
@@ -6,9 +8,13 @@ namespace HelloMVC.Tests
     public class HomeControllerTests
     {
         [Fact]
-        public void Test1()
+        public void Index_Returns_ViewResult()
         {
+            var controller = new HomeController();
 
+            var result = controller.Index();
+
+            Assert.IsType<ViewResult>(result);
         }
     }
 }
